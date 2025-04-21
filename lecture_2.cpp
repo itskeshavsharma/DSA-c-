b8d510ab519180f6674c8fb5373a01794db5001d
Keshav <keshavsharma7167@gmail.com>
2025-04-21 23:52:33 +0530

    Initial commit with lecture_2.cpp

---
+++ lecture_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double d= 1.23;
+    cout<<d<<endl;
+    float f= 1.2;
+    cout<<f<<endl;
+    int a = 'a';
+    cout << a << endl;
+    unsigned int h =-1;
+    cout<< h <<endl;   
+
+    int p=1;
+    int q= 2;
+    bool first = (p==q);
+    cout << first << endl;
+}
